6eea7aac45bbf0546ccc06f5ceaa3f256bec308e
ВашÐcokes238 <_cokes238@gmail.com>
2026-05-06 09:48:50 +0300

    Add random dice roll logic

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
@@ -11,11 +14,11 @@ class DiceRoller extends StatefulWidget {
 
 class _DiceRollerState extends State<DiceRoller> {
 
-  var activeDiceImage = 'assets/images/dice-1.png';
+  var currentDiceRoll = 2;
 
   void rollDice() {
     setState(() {
-      activeDiceImage = 'assets/images/dice-4.png';
+      currentDiceRoll = randomizer.nextInt(6) + 1;  
     });
     print('изменили картинку');
   }
@@ -25,7 +28,9 @@ class _DiceRollerState extends State<DiceRoller> {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Image.asset(activeDiceImage,width: 300,),
+        Image.asset(
+          'assets/images/dice-$currentDiceRoll.png',
+          width: 300,),
         TextButton(
           onPressed: rollDice,
           style: TextButton.styleFrom(
